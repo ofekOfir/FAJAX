cd78efb1ba175525fa508a8dca4cdf5c851c1e05
ofekOfir <ofekofir2001@gmail.com>
2022-12-14 18:41:52 +0200

    end database

---
+++ js/server.js
@@ -0,0 +1,20 @@
+class Server{
+    serving(linkObj){
+        if(method=="POST" && linkObj==){
+
+        }
+        else if(method=="post" && linkObj==){
+
+        }
+        else if(method=="post"){
+
+        }
+        else if(method=="post"){
+
+        }
+        else if(method=="get"){
+
+        }
+
+    }
+}
